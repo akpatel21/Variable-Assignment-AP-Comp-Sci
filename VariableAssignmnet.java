f7b0202b6b7fcd069aff7a5d6d6fdb7fb83a0cc0
akpatel21 <43002673+akpatel21@users.noreply.github.com>
2019-09-04 12:33:21 -0400

    Update VariableAssignmnet.java

--- VariableAssignmnet.java
+++ VariableAssignmnet.java
@@ -1,18 +1,12 @@
-import java.util.Scanner;
-
 public class VariableAssignment
 {
 
         public static void main(String[] args)
         {
-              Scanner scan = new Scanner(System.in);
-              System.out.print("What is your name? ");
-              String name = scan.nextLine();
-              System.out.print("What is yout age?" );
-              Int age = scan.nextInt();
-              System.out.print("What is your favorite subject? ");
-              String favSubject = scan.nextLine();
+              String name = "Akul Patel";
+              Int age = 16;
+              String favSubject = "Computer Science";
               
-              System.out.print("My name is " + name + "\n\tage: " + age + "\n\n\tfavorite subject: " + favSubject);
+              System.out.print("My name is " + name + "\n\n\tage: " + age + "\n\n\tfavorite subject: " + favSubject);
         }
 }
