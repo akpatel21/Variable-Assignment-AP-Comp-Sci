143c5f791a8802c27dea378b3e34164f031bd20f
akpatel21 <43002673+akpatel21@users.noreply.github.com>
2019-09-04 17:22:06 -0400

    Update VariableAssignmnet.java

--- VariableAssignmnet.java
+++ VariableAssignmnet.java
@@ -4,7 +4,7 @@ public class VariableAssignment
         public static void main(String[] args)
         {
               String name = "Akul Patel";
-              Int age = 16;
+              int age = 16;
               String favSubject = "Computer Science";
               
               System.out.print("My name is " + name + "\n\n\tage: " + age + "\n\n\tfavorite subject: " + favSubject);
